b39243b1c1266c18e00f90f18f7ba605de576255
Kelsey Thang <thang.kelsey@gmail.com>
2023-04-26 22:37:47 -0400

    Update account settings

--- client/src/pages/profile/AccountSettings.jsx
+++ client/src/pages/profile/AccountSettings.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect, useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { UserContext } from '../../contexts/UserContext';
 import { Alert, Box, Button, Divider, FormControl, TextField, Typography } from '@mui/material';
 import LoginForm from './LoginForm';
@@ -16,7 +15,6 @@ const AccountSettings = () => {
     }
   }
   const [formData, setFormData] = useState(initialState);
-  const navigate = useNavigate();
 
   useEffect(() => {
     if (user) {
