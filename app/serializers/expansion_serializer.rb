90d05ff90799e3aedd94b4fabf038d44164836af
Kelsey Thang <thang.kelsey@gmail.com>
2023-05-03 16:53:20 -0400

    add total cards to expansions controller

--- app/serializers/expansion_serializer.rb
+++ app/serializers/expansion_serializer.rb
@@ -1,10 +1,14 @@
 class ExpansionSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
-  attributes :id, :name, :release_date, :total_cards, :normal, :normal_foil, :special, :special_foil, :full_art, :full_art_foil, :image_url
+  attributes :id, :name, :release_date, :total_cards, :normal, :normal_foil, :special, :special_foil, :full_art, :full_art_foil, :total_cards, :image_url
   
   def image_url
     if object.image.attached?
       rails_blob_url(object.image, only_path: true)
     end
+  end
+
+  def total_cards
+    object.normal + object.normal_foil + object.special + object.special_foil + object.full_art + object.full_art_foil
   end
 end
