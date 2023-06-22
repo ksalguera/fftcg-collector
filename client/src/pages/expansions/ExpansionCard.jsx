0c7e7f2c94ec152d84271808bbf9a2a433aea68f
Kelsey Thang <thang.kelsey@gmail.com>
2023-06-21 20:35:43 -0400

    update linear progress

--- client/src/pages/expansions/ExpansionCard.jsx
+++ client/src/pages/expansions/ExpansionCard.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Box, Button, Card, CardContent, CardMedia, LinearProgress, Typography } from '@mui/material';
 import SearchIcon from '@mui/icons-material/ManageSearch';
 import { useMode } from '../../contexts/ThemeContext';
+import { AppContext } from '../../contexts/AppContext';
 
 const ExpansionCard = ({ name, releaseDate, image, stats }) => {
   const navigate = useNavigate();
+  const { user } = useContext(AppContext);
   const [theme] = useMode();
 
   return (
@@ -14,6 +16,7 @@ const ExpansionCard = ({ name, releaseDate, image, stats }) => {
         <CardContent sx={{ flex: '1 0 auto' }}>
           <Typography variant='h3' color={theme.palette.mode === 'light' ? 'secondary.dark' : 'secondary.main'}>{name}</Typography>
           <Typography sx={{ fontSize: 14 }} mb={4} color='text.secondary'>Released: {new Date(releaseDate).toLocaleDateString()}</Typography>
+          { user && 
           <Box mb={2} sx={{ display: 'flex', alignItems: 'center' }}>
             <Box sx={{ width: '80%', mr: 1 }}>
               <LinearProgress variant='determinate' value={stats} sx={{ height: 8, borderRadius: 5 }} />
@@ -21,7 +24,7 @@ const ExpansionCard = ({ name, releaseDate, image, stats }) => {
             <Box sx={{ minWidth: 35 }}>
               <Typography variant='body2' color="text.secondary">{stats} %</Typography>
             </Box>
-          </Box>
+          </Box> }
           <Button size='small' variant='outlined' onClick={() => navigate(`/browse-sets/${name}`)} endIcon={<SearchIcon />}>View Details</Button>
         </CardContent>
         <CardMedia sx={{ width: '50%'}} image={image} />
