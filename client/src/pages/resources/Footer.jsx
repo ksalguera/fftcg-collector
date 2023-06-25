9d14516770c9884c57a0b38ead1606854444d901
Kelsey Thang <thang.kelsey@gmail.com>
2023-06-25 17:25:45 -0400

    update rendering of disclaimer

--- client/src/pages/resources/Footer.jsx
+++ client/src/pages/resources/Footer.jsx
@@ -8,19 +8,22 @@ const Footer = () => {
   const location = useLocation();
 
   return (
-    <Box sx={{ left: 0, right: 0, bgcolor: location.pathname === '/' ? 'primary.main' : '', color: location.pathname === '/' ? 'white' : '' }}>
-      <Container maxWidth='xl' sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        <Stack>
-          <Typography variant='caption' mb={1}>
-            Information presented on this website about the Final Fantasy Trading Card Game, including FINAL FANTASY, SQUARE ENIX,
-            the SQUARE ENIX logo, images, and text, are registered trademarks of Square Enix Holdings Co, Ltd. FFTCG Collector is 
-            not affiliated with Square Enix Holdings Co, Ltd. 
-          </Typography>
-          <Typography variant='caption' mb={1}>
-            Copyright © 2023 FFTCG Collector
-          </Typography>
-        </Stack>
-      </Container>
+    <Box sx={{ left: 0, right: 0 }}>
+      {
+        location.pathname === '/' ? null :
+        <Container maxWidth='xl' sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <Stack>
+            <Typography variant='caption' mb={1}>
+              Information presented on this website about the Final Fantasy Trading Card Game, including FINAL FANTASY, SQUARE ENIX,
+              the SQUARE ENIX logo, images, and text, are registered trademarks of Square Enix Holdings Co, Ltd. FFTCG Collector is 
+              not affiliated with Square Enix Holdings Co, Ltd. 
+            </Typography>
+            <Typography variant='caption' mb={1}>
+              Copyright © 2023 FFTCG Collector
+            </Typography>
+          </Stack>
+        </Container>
+      }
     </Box>
   )
 }
